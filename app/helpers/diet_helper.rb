7efcb0af6a041eb1cb3bb40424e5436f0d8c9a32
tomazrvb <tomazrvb@hotmail.com>
2020-03-18 21:09:08 -0300

    Add hlpers to format date and time

--- app/helpers/diet_helper.rb
+++ app/helpers/diet_helper.rb
@@ -1,2 +1,15 @@
 module DietHelper
+    def date_brazilian_formates(date)
+        date = date.to_s.split('-')
+        formated_list = []
+        formated_list << date[2]
+        formated_list << date[1]
+        formated_list << date[0]
+        return formated_list.join('/')
+    end
+    def time_brazilian_formates(time)
+        time = time.to_s.split(' ')
+        formated_time = time[1]
+        return formated_list
+    end
 end
