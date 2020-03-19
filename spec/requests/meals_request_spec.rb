1d13b4845779a68eafc5bc76e75886bdd8ea021d
tomazrvb <tomazrvb@hotmail.com>
2020-03-19 17:22:21 -0300

    Criate testes to user controller

--- spec/requests/meals_request_spec.rb
+++ spec/requests/meals_request_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Meals", type: :request do
+    before do
+        @user = User.create(name:"Maida", email:"Maida@infoway.com", password: "draft1", password_confirmation: "draft1")
+        @diet = Diet.create(start_date: "2020-03-31",end_date: "2020-03-31",starting_weight: 108,ideal_weight: 80,user_id: @user.id,height: 1.8)
+      end
+      let(:valid_meal) {
+        { :schedule => "04:00",:meal_type => "1",:description =>" Bla Bla Bla"} 
+      }
 
+    describe "POST #index" do
+        it "returns a success if response can create a meal" do
+            post login_path, params: { session:{email:"Maida@infoway.com",password: "draft1"} }
+            post meals_path, params: { meal:valid_meal }
+            expect(response).to redirect_to home_path
+            expect(response.body).not_to match /Dados incompletos ou faltantes/im
+        end
+    end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
+    describe "GET #User" do
+        it "returns a success with get create viw" do
+            get new_user_path
+            expect(response).to be_successful
+        end
+    end
+    describe "Post #User" do
+        it "returns a success with get create viw" do
+            post users_path, params: { user:{ :name => "Maida", :email => "maida-healt@infoway.com", :password => "draft1",:password_confirmation => "draft1" }}
+            expect(response).to redirect_to home_path
+        end
+        it "returns a success with get create viw" do
+            post users_path, params: { user:{ :name => "Maida", :password => "draft1",:password_confirmation => "draft1" }}
+            expect(response.body).to match /Dados incompletos ou faltantes/im
+        end
+    end
 end
